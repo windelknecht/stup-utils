e390c21209bc9d37e98b0e769383da380834c861
Heiko Blobner <h.blobner@avm.de>
2014-09-04 14:33:27 +0200

    [build]   {enh} .akka-actor -> 2.3.5   {enh} .scalatest -> 2.2.2   {enh} .all libs now with sources

--- project/build.scala
+++ project/build.scala
@@ -1,11 +1,10 @@
 import sbt._
 import Keys._
 
-object Build extends Build {
-  lazy val scalaVer = "2.11.1"
+object build extends Build {
+  lazy val scalaVer = "2.11.2"
 
   lazy val defaultSettings =
-    Defaults.defaultSettings ++
     Seq(
       organization := "de.windelknecht",
       version := guessVersion,
@@ -25,23 +24,25 @@ object Build extends Build {
     file("."),
     settings = defaultSettings ++ Seq(
       libraryDependencies ++= Seq(
-        "org.scala-lang"                % "scala-library"           % scalaVer,
-        "org.scala-lang"                % "scala-reflect"           % scalaVer,
-        "org.scala-lang.modules"        % "scala-xml_2.11"          % "1.0.2",
-        "com.typesafe.akka"             % "akka-actor_2.11"         % "2.3.3",
+        "org.scala-lang"                % "scala-library"           % scalaVer                  withSources(),
+        "org.scala-lang"                % "scala-reflect"           % scalaVer                  withSources(),
+        "org.scala-lang.modules"        % "scala-xml_2.11"          % "1.0.2"                   withSources(),
+        "com.typesafe.akka"             % "akka-actor_2.11"         % "2.3.5"                   withSources(),
         // apache
-        "org.apache.commons"            % "commons-compress"        % "1.8.1",
-        "org.apache.commons"            % "commons-vfs2"            % "2.0",
+        "org.apache.commons"            % "commons-compress"        % "1.8.1"                   withSources(),
+        "org.apache.commons"            % "commons-vfs2"            % "2.0"                     withSources(),
         // logging
-        "org.clapper"                   % "grizzled-slf4j_2.11"     % "1.0.2",
+        "org.clapper"                   % "grizzled-slf4j_2.11"     % "1.0.2"                   withSources(),
         // refl
-        "org.reflections"               % "reflections"             % "0.9.9-RC1",
+        "org.reflections"               % "reflections"             % "0.9.9-RC1"               withSources(),
         // testing
-        "org.scalatest"                 % "scalatest_2.11"          % "2.2.0"     % "test",
-        "com.typesafe.akka"             % "akka-testkit_2.11"       % "2.3.3"     % "test",
-        "junit"                         % "junit"                   % "4.11"      % "test",
-        "org.mockito"                   % "mockito-all"             % "1.9.5"     % "test"
-      )))
+        "org.scalatest"                 % "scalatest_2.11"          % "2.2.2"     % "test"      withSources(),
+        "com.typesafe.akka"             % "akka-testkit_2.11"       % "2.3.5"     % "test"      withSources(),
+        "junit"                         % "junit"                   % "4.11"      % "test"      withSources(),
+        "org.mockito"                   % "mockito-all"             % "1.9.5"     % "test"      withSources()
+      ),
+      fork in Test := true
+      ))
 
   lazy val testPlugin = Project(
     "testPlugin",
