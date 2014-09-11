59a88a3f45ba53f6ed312abf8be4279114c59ee6
Heiko Blobner <heiko.blobner@gmx.de>
2014-09-11 22:14:27 +0200

    [ui]   {enh} .PopupWatcher: singleton to show only one popup at time

---
+++ src/main/scala/de/windelknecht/stup/utils/ui/PopupWatcher.scala
@@ -0,0 +1,45 @@
+package de.windelknecht.stup.utils.ui
+
+/**
+ * Created by me.
+ * User: Heiko Blobner
+ * Mail: heiko.blobner@gmx.de
+ *
+ * Date: 11.09.14
+ * Time: 21:41
+ */
+object PopupWatcher {
+  type PopupHideFunc[T] = { def hide(): Unit; def show(arg: T): Unit }
+
+  // fields
+  private var _activePopup: Option[PopupHideFunc[_]] = None
+
+  /**
+   * Close currently open popup.
+   */
+  def close(): Unit = synchronized { hide() }
+
+  /**
+   * Activate the given popup.
+   */
+  def show[T](popup: PopupHideFunc[T], arg: T): Unit = {
+    synchronized {
+      hide()
+      _activePopup = Some(popup)
+      popup.show(arg)
+    }
+  }
+
+  /**
+   * Hide active popup.
+   *
+   * THIS METHOD IS NOT REENTRANT SAFE!!!
+   */
+  private def hide(): Unit = {
+    _activePopup match {
+      case Some(x) => x.hide()
+      case None =>
+    }
+    _activePopup = None
+  }
+}
